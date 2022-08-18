de0942df623fb75859d863d4f763a8f44f8a6476
Viacheslav <davidov.ve@yandex.ru>
2022-08-18 09:56:09 +0300

    task 29 homework

---
+++ task29_homework/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+Console.WriteLine("Вывод массива из 8 элементов: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 11);
+    Console.Write(array[i]);
+    Console.Write(", ");
+}
